bc42f6f8ce47a5398752f9af9212eb03fe0646df
MrOtaiLy <lykovvana824@gmail.com>
2025-02-25 17:34:19 +0300

    Изображение Марса/Реклама с картинкой

--- ImageOfMars.py
+++ ImageOfMars.py
@@ -40,6 +40,36 @@ def image_mars():
   </body>
 </html>"""
 
+@app.route('/promotion_image')
+def promotion_image():
+    return f"""<!doctype html>
+<html lang="ru">
+  <head>
+    <meta charset="utf-8">
+    <title>Колонизация Марса</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="{url_for('static', filename='css/style.css')}">
+  </head>
+  <body>
+    <h1>Жди нас, Марс!</h1>
+    <div class="alert alert-dark" role="alert">
+      Человечество вырастает из детства.
+    </div>
+    <div class="alert alert-success" role="alert">
+      Человечеству мала одна планета.
+    </div>
+    <div class="alert alert-secondary" role="alert">
+      Мы сделаем обитаемыми безжизненные пока планеты.
+    </div>
+    <div class="alert alert-warning" role="alert">
+      И начнем с Марса!
+    </div>
+    <div class="alert alert-danger" role="alert">
+      Присоединяйся!
+    </div>
+    <img src="{url_for('static', filename='img/mars.png')}" alt="Mars" class="img-fluid">
+  </body>
+</html>"""
 
 if __name__ == "__main__":
     app.run(port=8080, host="127.0.0.1")
